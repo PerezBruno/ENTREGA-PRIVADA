cf4b08568097a228dd8551fbdd04b6704f19b710
PerezBruno <brunoleandroperez@hotmail.com>
2023-03-05 17:53:54 -0300

    iniciando entrega clase 8

--- src/app.js
+++ src/app.js
@@ -6,17 +6,26 @@ const ProductManager = require("./ProductManager")
 
 const { Server } = require("socket.io")
 
+const displayRoutes = require("express-routemap");
+
+//const corsConfig = require("./config/cors.config");
+
+const mongoose = require("mongoose")
+//const { connect } = require("mongoose");
 
 const productRoutes = require("./routes/product.routes")
 
 const cartsRoutes = require("./routes/carts.routes")
 
-const handlebars = require("express-handlebars")
+const handlebars = require("express-handlebars");
+const { DB_HOST, DB_PORT, DB_NAME, DB_CNN } = require("./config/config");
 
 const productManager = new ProductManager("./products/productos.json");
 
 
 const BASE_PREFIX = "api";
+
+const uri = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
 
 const PORT = 8080;
@@ -63,40 +72,67 @@ app.get("/", async (req, res) => {
 // configurando Socket.io
 
 const server = app.listen(PORT, ()=>{
+  displayRoutes(app)
   console.log(`Escuchando en puerto ${PORT}`)
 })
 
 const io = new Server(server);
 
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
 //  console.log("🚀 ~ file: app.js:73 ~ io.on ~ socket:", socket)
   console.log("conectado")
+
+ // const products = await productManager.getProducts();
+	socket.emit('products', listaProducts);
+
+	socket.on('addProd', async prod => await productManager.addProducts(prod));
+
+	socket.on('delProd', async id => await productManager.deleteProduct(id) && console.log(id));
   	
 	})
 
 
+  // ****** Configuración de mongoose *****
+  const connection = mongoose;
 
-//***********************LLEGUE AL MINUTO 28 DEL VIDEO 3 ***********************/
+  connection.connect(uri)
+  .then((conn) => {
+    console.log("🚀 ~ file: app.js:18 ~ CONECTADO!:");
+  })
+  .catch((err) => {
+    console.log("🚀 ~ file: app.js:20 ~ err:", err);
+  });
+
+  // const configConnection = {
+  //   url: DB_CNN ?? `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+  //   options: {
+  //     useNewUrlParser: true,
+  //     useUnifiedTopology: true,
+  //   },
+  // };
+
+  // const mongoDBconnection = async () => {
+  //   try {
+  //     await connect(configConnection.url, configConnection.options);
+  //     console.log(`=================================`);
+  //     console.log(
+  //       `======= URL: ${configConnection.url.substring(0, 20)} =======`
+  //     );
+  //     console.log(`=================================`);
+  //   } catch (error) {
+  //     console.log(
+  //       "🚀 ~ file: mongo.config.js:23 ~ mongoDBconnection ~ error:",
+  //       error
+  //     );
+  //     throw new Error(error);
+  //   }
+  // };
 
 
 
 
-
-
-//Configurar e instalar handlebars - listo
-
-
-// Configurar e instalar socket
-
-
-//Crear vista home.handlebars debe mostrar todos los productos agregados hasta el momento
-
-
-//Crear la vista realTimeProducts.handlebars con el endpoint "/realtimeproducts" en nuestro views router
-//Debe contener la misma lista de productod pero debe trabajar con websocket
-//Cada vez que se actualice la lista de productos se debe actualizar la vista automáticamente
-
-//Para manipular las listas de productos, crear una vista simple de un formulario en la vista realTimeProducts.handlebars
-
-//Si se desea hacer la conexion de socket emits con HTTP, deberas buscar la forma de utilizar el servidor "io" de sockets dentro de la petición POST
+  /********************************************************
+   * 
+   * base de datos conecta
+   */
