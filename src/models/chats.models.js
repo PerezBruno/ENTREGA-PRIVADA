d7870600fad4095cc11e99e907c7ac651880f822
Bruno <brunoleandroperez@hotmail.com>
2023-03-10 02:02:36 -0300

    09/03 - prueba de postman en rutas de chat satisfactoria

--- src/models/chats.models.js
+++ src/models/chats.models.js
@@ -6,7 +6,6 @@ const messagesSchema = new mongoose.Schema({
     user:{
         type: String,
         required: true,
-        unique: true,
     },
     message: {
         type: String,
--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -1,35 +1,49 @@
 const { Router } = require("express");
 const router = Router();
+const chatsModel = require("../models/chats.models")
 
-// class ChatRoutes {
-//     path = "/chat";
-//     router = Router();
+class ChatRoutes {
+  path = "/chat";
+  router = Router();
 
-//     constructor(){
-//         this.initChatsRoutes();
-//       }
+  constructor() {
+    this.initChatsRoutes();
+  }
 
-//       initChatsRoutes(){
-//         this.router.get(`${this.path}`, async (req, res)=>{
-
-//         })
-//       }
-// }
+  initChatsRoutes() {
+    this.router.get(`${this.path}`, async (req, res) => {});
+  }
+}
 
 router.get("/", async (req, res) => {
-  //     try {
-  //         const result = await chatModel.find();
-  //     } catch (error) {
-  //         console.log("🚀 ~ file: chat.routes.js:23 ~ router.get ~ error:", error)
-  //     }
+  try {
+    const messages = await chatsModel.find();
+
+    //++++++++++ AGREGAR VALIDACIONES +++++++++++++++/////
+
+    return res.json({
+      message: "mensajes obtenidos satisfactoriamente",
+      messages,
+    });
+  } catch (error) {
+  console.log("🚀 ~ file: chat.routes.js:28 ~ router.get ~ error:", error)
+  }
 });
 
 router.post("/", async (req, res) => {
-  //     try {
-  //         const result = await chatModel.find();
-  //     } catch (error) {
-  //         console.log("🚀 ~ file: chat.routes.js:23 ~ router.get ~ error:", error)
-  //     }
+  try {
+    //++++++HACER VALIDACIONES y manejar errores++++++++
+    const messageBody = req.body;
+    const newMessage = await chatsModel.create(messageBody);
+
+    return res.json({
+      message: `message added succesfully`,
+      message: newMessage,
+    });
+  } catch (error) {
+  console.log("🚀 ~ file: chat.routes.js:43 ~ router.post ~ error:", error)
+
+  }
 });
 
 module.exports = router;
